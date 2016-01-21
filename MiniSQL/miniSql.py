33079bb17892c1aaf95f0bbce0877391b657122f
NightFury13 <develop13mohit@gmail.com>
2016-01-21 18:30:04 +0530

    loading database complete

--- MiniSQL/miniSql.py
+++ MiniSQL/miniSql.py
@@ -1,22 +1,87 @@
 ######################################
+# MiniSQL - A mini database system   #
 # @Author : Mohit Jain               #
 # @Email  : develop13mohit@gmail.com #
 ######################################
 
 import os
 import sys
+from termcolor import colored, cprint
+import json
+import csv
 
 def fetchFiles(path):
+    """
+    Check if path provided exists. If so, load the data files.
+    """
     if not os.path.isdir(path):
-        print "[ERROR] Invalid path, does not exist... ", path
+        print colored("[ERROR]",'red'),"Invalid path, does not exist... ", path
         print "Try again?\n Path : ",
-        path = raw_input()
-        fetchFiles(path)
+        new_path = raw_input()
+        new_path, new_data = fetchFiles(new_path)
+        return new_path, new_data
     else:
         data_files = os.listdir(path)
-        print "[INFO] Found %s database... Now loading." % len(data_files)
-        return data_files
-        
+        print colored("[INFO]",'green')+" Found %s files... Now populating database." % len(data_files)
+        return path, data_files
+
+def groupGenerator(seq, delimitor):
+    """
+    Group the elements of a list based on a delimiter word.
+    """
+    group = []
+    for ele in seq:
+        if ele==delimitor:
+            yield group
+            group = []
+        group.append(ele)
+    yield group
+
+def loadDatabases(path, data_files):
+    """
+    Check if the metadata file exists and load the table contents accordingly.
+    """
+    if "metadata.txt" not in data_files:
+        print colored("[ERROR]",'red'),"Metadata file (metadata.txt) not found in provided database path!"
+        return "error"
+    else:
+        try:
+            with open(path+'/metadata.txt','r') as meta_file:
+                meta_content = meta_file.read().splitlines()
+                tables = filter(None, list(groupGenerator(meta_content, "<begin_table>")))
+                meta_tables = {}
+                for table in tables:
+                    t_name = table[1]
+                    meta_tables[t_name] = {}
+                    for col in table[2:-1]:
+                        meta_tables[t_name][col] = []
+            print 
+            print colored("> Database Schema : ","yellow")
+            print colored(json.dumps(meta_tables,sort_keys=True, indent=4),'cyan')
+
+            # Fill the data values
+            for table_f in data_files:
+                if table_f=='metadata.txt':
+                    continue
+                else:
+                    with open(path+'/'+table_f,'r') as tab_f:
+                        t_name = table_f.split('.')[0]
+                        table_content = [row for row in csv.reader(tab_f, delimiter=',',skipinitialspace=True)]
+                        header, contents = table_content[0], table_content[1:]
+                        for row in contents:
+                            for col in meta_tables[t_name]:
+                                meta_tables[t_name][col].append(int(row[header.index(col)]))
+            return meta_tables
+        except:
+            print "[ERROR] Metadata file maybe corrupt! Format mismatch."
+            return "error"
+
+def startEngine():
+    """
+    Main controller function for taking query inputs and displaying respective outputs.
+    """
+    return 0
+
 def main():
     """
     Initializer Function.
@@ -27,7 +92,16 @@ def main():
     # Initialze Database.
     print "Please enter path to the database files :",
     path = raw_input()
-    databases = fetchFiles(path)
+    path, data_files = fetchFiles(path)
+    database = loadDatabases(path, data_files)
+    if database=="error":
+        print 
+        print colored('Review the errors and try again','yellow')
+        return 
+
+    # Start the query engine.
+    startEngine()
+
 
 if __name__=='__main__':
     main()
